50d4c4f269a973da0647fefdbb11d94cde6e18c7
afc11hn <afc11hn@hotmail.de>
2017-05-08 18:08:52 +0200

    Make getdata 2.2% faster (getdata2)

--- files.py
+++ files.py
@@ -7,3 +7,30 @@ def getdata(file_path):
         names.append(namestemp.replace("\n", ""))
         links.append(linkstemp.replace("\n", ""))
     return names, links
+
+
+def getdata2(file_path):
+    names, links = [], []
+    with open(file_path) as f:
+        for i in f.read().splitlines():
+            namestemp, linkstemp = i.split("~")
+            names.append(namestemp)
+            links.append(linkstemp)
+    return names, links
+
+
+import time
+
+i = 0
+start = time.time()
+while time.time() - start < 1:
+    getdata('data.txt')
+    i += 1
+print(i)  # around 9150/s
+
+i = 0
+start = time.time()
+while time.time() - start < 1:
+    getdata2('data.txt')
+    i += 1
+print(i)  # around 9350/s
